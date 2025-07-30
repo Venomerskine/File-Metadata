7979dbc2d0aeb3dc318756bcde34dac53e3e346d
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-07-30 20:27:51 +0300

    try 1

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+const bodyparser = require('body-parser');
+const multer = require('multer');
 require('dotenv').config()
+
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
 app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
+  console.log(req.body);
+ res.json({success: true})
+})
 
 
 const port = process.env.PORT || 3000;
